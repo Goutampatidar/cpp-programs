1afa5fca55fe3bf763ea739c4c25160c10c300b1
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-01-14 21:08:18 +0530

    Add files via upload

---
+++ pattern 5 .cpp
@@ -0,0 +1,29 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,k,j,i;
+    cout<<"enter the no. ";
+    cin>>a;
+    for(i=a; i>=1; i--)
+    {
+
+        for(j=a; j>=i; j--)
+        {
+            cout<<" ";
+        }
+        for(k=i; k>=1 ; k--)
+        {
+            cout<<"*";
+
+        }
+
+        cout<<" \n";
+    }
+
+    getch();
+}
+
