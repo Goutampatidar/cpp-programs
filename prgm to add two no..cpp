fc3aff2b94ae4407290303f056afae649d31d7ef
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-01-19 13:36:05 +0530

    Add files via upload

---
+++ prgm to add two no..cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b,sum;
+    cout<<"enter any two no\n";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"sum ="<<sum;
+    getch();
+}
+
