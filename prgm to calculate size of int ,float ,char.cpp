634c7bc4618607388301aa45c1fe8f52798185c6
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-01-27 13:23:05 +0530

    Add files via upload

---
+++ prgm to calculate size of int ,float ,char.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+int  main()
+{
+    int a;
+    char b;
+    float c;
+    long d;
+    double e;
+    cout<<"size of int ="<<sizeof(a)<<endl;
+    cout<<"size of char="<<sizeof(b)<<endl;
+    cout<<"size of float"<<sizeof(c)<<endl;
+    cout<<"size of long"<<sizeof(d)<<endl;
+    cout<<"size of double "<<sizeof(e)<<endl;
+    getch();
+}
