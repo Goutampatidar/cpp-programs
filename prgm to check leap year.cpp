e3d397fff780bc4ddceea197bd7563436849c39f
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-02-03 20:30:29 +0530

    Add files via upload

---
+++ prgm to check leap year.cpp
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+ int main()
+ {
+     int a;
+     cout<<"enterr the year";
+     cin>>a;
+
+     if(a%4==0)
+     {
+         cout<<"year is leap year";
+
+     }
+     else
+     {
+         cout<<"year is not leap year";
+     }
+     getch();
+
+ }
