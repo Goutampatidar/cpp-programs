8bd1f2d7c865bfdb143234f24775898a67002cdf
Goutam Patidar <73788341+Goutampatidar@users.noreply.github.com>
2021-02-16 14:45:40 +0530

    Add files via upload

---
+++ prgm to find quotient and reminder.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+    int a,b,r,q;
+    cout<<"enter divident and divisior\n";
+    cin>>a>>b;
+    r=a%b;
+    q=a/b;
+    cout<<"reminder="<<r;
+    cout<<"quotient ="<<q;
+    getch();
+}
+
